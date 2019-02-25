cda66f67bd7cd74e45dd0c145a88b7788414b60c
h2ck4u <h2ckforu@gmail.com>
2019-02-25 23:11:31 +0900

    : :bulb: id,pw 클릭시 이벤트 프로토타이핑

--- popup.js
+++ popup.js
@@ -11,7 +11,47 @@ document.getElementById("addConfig").onclick = function () {
       });
     });
   });
-  // window.close();
+}
+
+document.getElementById("labelId").onclick = function () {
+  chrome.tabs.query({
+    active: true,
+    currentWindow: true
+  }, function (tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, {
+      type: "getIdInput"
+    }, function (response) {
+      console.log(response);
+    });
+  });
+}
+
+document.getElementById("labelPw").onclick = function () {
+  chrome.tabs.query({
+    active: true,
+    currentWindow: true
+  }, function (tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, {
+      type: "getPwInput"
+    }, function (response) {
+      console.log(response);
+    });
+  });
+}
+
+document.getElementById("addConfig").onclick = function () {
+  chrome.tabs.query({
+    active: true,
+    currentWindow: true
+  }, function (tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, {
+      type: "getDomain"
+    }, function (domain) {
+      getStorageData('data').then(function (data) {
+        updateStroageData(data, domain);
+      });
+    });
+  });
 }
 
 function getStorageData(key) {
